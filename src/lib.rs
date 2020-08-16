21ef4898b479a263c4e6b752f3f4465ca2a781f6
Alex M. M <acdenissk69@gmail.com>
2020-08-17 01:29:59 +0200

    Dispatch

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use serenity::prelude::{Context as SerenityContext, RwLock};
 use serenity::model::prelude::Message;
 use serenity::futures::future::{BoxFuture, FutureExt};
+use serenity::http::Http;
+use serenity::cache::Cache;
 
 use std::sync::Arc;
 use std::error::Error as StdError;
@@ -13,12 +15,21 @@ pub type DefaultError = Box<dyn StdError + Send + Sync>;
 #[derive(Clone)]
 pub struct Context<D = DefaultData> {
     pub data: Arc<RwLock<D>>,
-    pub msg: Message,
     parent_ctx: SerenityContext,
 }
 
+impl<D> Context<D> {
+    pub fn http(&self) -> Arc<Http> {
+        self.parent_ctx.http.clone()
+    }
+
+    pub fn cache(&self) -> Arc<Cache> {
+        self.parent_ctx.cache.clone()
+    }
+}
+
 pub type CommandResult<E = DefaultError> = std::result::Result<(), E>;
-pub type CommandFn<D = DefaultData, E = DefaultError> = fn(ctx: Context<D>) -> BoxFuture<'static, CommandResult<E>>;
+pub type CommandFn<D = DefaultData, E = DefaultError> = fn(ctx: Context<D>, msg: Message) -> BoxFuture<'static, CommandResult<E>>;
 
 pub type CommandConstructor<D = DefaultData, E = DefaultError> = fn() -> Command<D, E>;
 
@@ -114,6 +125,8 @@ impl<D, E> GroupBuilder<D, E> {
 
         let group = group();
 
+        assert!(!group.prefixes.is_empty(), "subgroups cannot have zero prefixes");
+
         for prefix in &group.prefixes {
             self.inner.subgroup_prefix_to_id.insert(prefix.clone(), id);
         }
@@ -128,11 +141,33 @@ impl<D, E> GroupBuilder<D, E> {
     }
 }
 
+#[non_exhaustive]
+#[derive(Debug, Default, Clone)]
+pub struct Configuration {
+    pub prefix: String,
+}
+
+impl Configuration {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn prefix<I>(mut self, prefix: I) -> Self
+    where
+        I: Into<String>
+    {
+        self.prefix = prefix.into();
+        self
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct Framework<D = DefaultData, E = DefaultError> {
+    pub conf: Configuration,
     pub data: Arc<RwLock<D>>,
     pub group_prefix_to_id: HashMap<String, GroupId>,
     pub groups: HashMap<GroupId, Group<D, E>>,
+    pub groups_without_prefixes: Vec<Group<D, E>>,
     _error: PhantomData<E>,
 }
 
@@ -157,17 +192,29 @@ where
 impl<D, E> Framework<D, E> {
     pub fn with_data(data: D) -> Self {
         Self {
+            conf: Configuration::default(),
             data: Arc::new(RwLock::new(data)),
             group_prefix_to_id: HashMap::default(),
             groups: HashMap::default(),
+            groups_without_prefixes: Vec::default(),
             _error: PhantomData,
         }
+    }
+
+    pub fn configuration(&mut self, conf: Configuration) -> &mut Self {
+        self.conf = conf;
+        self
     }
 
     pub fn group(&mut self, group: GroupConstructor<D, E>) -> &mut Self {
         let id = GroupId::from(group);
 
         let group = group();
+
+        if group.prefixes.is_empty() {
+            self.groups_without_prefixes.push(group);
+            return self;
+        }
 
         for prefix in &group.prefixes {
             self.group_prefix_to_id.insert(prefix.clone(), id);
@@ -176,6 +223,63 @@ impl<D, E> Framework<D, E> {
         self.groups.insert(id, group);
 
         self
+    }
+
+    pub async fn dispatch(&self, ctx: SerenityContext, msg: Message) -> Result<(), ()> {
+        let mut stream = uwl::Stream::new(msg.content.trim());
+
+        // Check for the presence of the prefix.
+        if stream.advance(self.conf.prefix.len()) != self.conf.prefix {
+            return Err(());
+        }
+
+        // Prefix is present, but no more information is available.
+        if stream.is_empty() {
+            return Err(());
+        }
+
+        let prefix_or_name = stream.take_until(|b| b == b' ');
+
+        // Ignore whitespace after the group prefix or command name.
+        stream.take_while_char(char::is_whitespace);
+
+        let command = if let Some(id) = self.group_prefix_to_id.get(prefix_or_name) {
+            let group = &self.groups[id];
+
+            // TODO: Subgroups
+
+            let name = stream.take_until(|b| b == b' ');
+
+            stream.take_while_char(char::is_whitespace);
+
+            let id = match group.command_name_to_id.get(name) {
+                Some(id) => id,
+                None => return Err(()),
+            };
+
+            &group.commands[id]
+        } else {
+            let mut iter = self.groups_without_prefixes.iter();
+            loop {
+                let group = match iter.next() {
+                    Some(group) => group,
+                    None => return Err(()),
+                };
+
+                if let Some(id) = group.command_name_to_id.get(prefix_or_name) {
+                    break &group.commands[id];
+                }
+            }
+        };
+
+        let ctx = Context {
+            data: self.data.clone(),
+            parent_ctx: ctx,
+        };
+
+        assert!((command.function)(ctx, msg).await.is_ok());
+
+        Ok(())
     }
 }
 
@@ -188,9 +292,8 @@ mod tests {
         text: String,
     }
 
-    fn ping(ctx: Context<TestData>) -> BoxFuture<'static, CommandResult> {
+    fn ping(ctx: Context<TestData>, _msg: Message) -> BoxFuture<'static, CommandResult> {
         async move {
-            println!("Hello world!");
             println!("{:?}", ctx.data.read().await.text);
             Ok(())
         }.boxed()
