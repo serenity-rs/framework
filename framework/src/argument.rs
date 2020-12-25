fff20c7c94fb6b7073b767df5b38f32c24ba07a9
Alex M. M <acdenissk69@gmail.com>
2020-12-25 09:14:01 +0100

    Add parsing functions for optional and variadic arguments

--- framework/src/argument.rs
+++ framework/src/argument.rs
@@ -1,4 +1,7 @@
+//! Utilities for parsing command arguments.
+
 use crate::context::Context;
+use crate::utils::ArgumentSegments;
 use crate::{DefaultData, DefaultError};
 
 use std::str::FromStr;
@@ -17,11 +20,48 @@ pub trait Argument<D = DefaultData, E = DefaultError>: Sized {
 
 impl<T, D, E> Argument<D, E> for T
 where
-    T: FromStr
+    T: FromStr,
 {
     type Error = <T as FromStr>::Err;
 
     fn parse(_: &Context<D, E>, source: &str) -> Result<Self, Self::Error> {
         <T as FromStr>::from_str(source)
     }
+}
+
+/// Tries to take a single segment from [a list of segments][asegs] and parse
+/// [an argument][arg] out of it.
+///
+/// If the list of segments is empty, `Ok(None)` is returned. Otherwise,
+/// the first segment is taken and parsed into an argument. If parsing succeeds,
+/// `Ok(Some(...))` is returned, otherwise `Err`.
+///
+/// [asegs]: crate::utils::ArgumentSegments
+/// [arg]: Argument
+pub fn opt_argument<T, D, E>(
+    ctx: &Context<D, E>,
+    segments: &mut ArgumentSegments<'_>,
+) -> Result<Option<T>, T::Error>
+where
+    T: Argument<D, E>,
+{
+    segments.next().map(|seg| T::parse(ctx, seg)).transpose()
+}
+
+/// Tries to parse many [arguments][arg] from [a list of segments][asegs].
+///
+/// Each segment in the list is parsed into a vector of arguments. If parsing
+/// all segments succeeds, the vector is returned. Otherwise, the first error
+/// is returned.
+///
+/// [asegs]: crate::utils::ArgumentSegments
+/// [arg]: Argument
+pub fn var_arguments<T, D, E>(
+    ctx: &Context<D, E>,
+    segments: &mut ArgumentSegments<'_>,
+) -> Result<Vec<T>, T::Error>
+where
+    T: Argument<D, E>,
+{
+    segments.map(|seg| T::parse(ctx, seg)).collect()
 }
