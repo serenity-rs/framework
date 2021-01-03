15e848e9016146734f5e0c0b4df76ddf1350ec7b
acdenisSK <acdenisSK@users.noreply.github.com>
2021-01-03 20:52:04 +0000

    deploy: 31732af1c003c0d28e4d62886ee85eef3207976a

--- master/implementors/core/convert/trait.From.js
+++ master/implementors/core/convert/trait.From.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["serenity_framework"] = [{"text":"impl&lt;D&gt; From&lt;fn() -&gt; Command&lt;D&gt;&gt; for CommandId","synthetic":false,"types":[]},{"text":"impl From&lt;DispatchError&gt; for Error","synthetic":false,"types":[]}];
+implementors["serenity_framework"] = [{"text":"impl&lt;D, E&gt; From&lt;fn() -&gt; Command&lt;D, E&gt;&gt; for CommandId","synthetic":false,"types":[]},{"text":"impl&lt;E&gt; From&lt;DispatchError&gt; for Error&lt;E&gt;","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- master/implementors/std/error/trait.Error.js
+++ master/implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["serenity_framework"] = [{"text":"impl&lt;E:&nbsp;StdError + 'static&gt; Error for ArgumentError&lt;E&gt;","synthetic":false,"types":[]},{"text":"impl Error for Reason","synthetic":false,"types":[]},{"text":"impl Error for DispatchError","synthetic":false,"types":[]},{"text":"impl Error for Error","synthetic":false,"types":[]}];
+implementors["serenity_framework"] = [{"text":"impl&lt;E:&nbsp;StdError + 'static&gt; Error for ArgumentError&lt;E&gt;","synthetic":false,"types":[]},{"text":"impl Error for Reason","synthetic":false,"types":[]},{"text":"impl Error for DispatchError","synthetic":false,"types":[]},{"text":"impl&lt;E:&nbsp;StdError + 'static&gt; Error for Error&lt;E&gt;","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
